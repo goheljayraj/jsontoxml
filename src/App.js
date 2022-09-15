b16221a26ccfcb642a0e7706d2af04c6edb79911
goheljayraj <goheljayraj@gmail.com>
2022-09-15 14:17:08 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import MyDropzone from './components/MyDropzone/MyDropzone';
+
+function App() {
+  return (
+    <div>
+    Jayeaj
+    <MyDropzone/>
+
+    </div>
+  );
+}
+
+export default App;
